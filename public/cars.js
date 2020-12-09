6e9e4c78b3443b5c6025ccc97900a20494251a4f
Anna Chirapongse <achirapongse@gmail.com>
2020-12-08 21:16:58 -0500

    Updates to index.handlebars & car.js

---
+++ public/cars.js
@@ -0,0 +1,27 @@
+$(function () {
+    //Click event for adding a car
+    $(".car-form").on("search", function (event) {
+        // Make sure to preventDefault on a submit event.
+        event.preventDefault();
+        //Grab the car information form the form field.  
+        var newCar = {
+            car_location: $("#city").val().trim(),
+            car_make: $("#make").val().trim(),
+            car_model: $("#model").val().trim(),
+            car_year: $("#year").val().trim(),
+            car_milage: $("#milage").val().trim(),
+            car_purpose: false,
+        };
+        // Send the POST request using ajax.
+        $.ajax("/new-car", {
+            type: "POST",
+            data: newCar
+        }).then(
+            function () {
+                console.log("Added new Car");
+                // Reload the page to get the updated list
+                location.reload();
+            }
+        );
+    });
+})
